9d6585327ee496ebe5d1b54282b70370341a9575
hua0122 <1835591342@qq.com>
2018-06-12 13:37:53 +0800

    xx

--- app/index/common.php
+++ app/index/common.php
@@ -435,13 +435,6 @@ function curl_data($url,$data=[],$timeout=30,$useCert=[])
     return $handles;
 }
 
-function isImage($filename){
-    $types = '.gif|.jpeg|.jpg|.png|.bmp';//定义检查的图片类型
-    if(file_exists($filename)){
-        $info = getimagesize($filename);
-        $ext = image_type_to_extension($info['2']);
-        return stripos($types,$ext);
-    }else{
-        return false;
-    }
+function isImage($url){
+    return preg_match('/.*(\.png|\.jpg|\.jpeg|\.gif)$/', $url);
 }
